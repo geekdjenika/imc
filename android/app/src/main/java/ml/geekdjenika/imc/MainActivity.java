c0ea1656fafb9f18b9f2ac987ed453c9d2d182f0
Geek DJENIKA <djenikaa@gmail.com>
2023-05-13 12:49:02 +0000

    Importing project

---
+++ android/app/src/main/java/ml/geekdjenika/imc/MainActivity.java
@@ -0,0 +1,6 @@
+package ml.geekdjenika.imc;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Accueil extends StatefulWidget {
+  const Accueil({Key? key}) : super(key: key);
+
+  @override
+  State<Accueil> createState() => _AccueilState();
+}
+
+class _AccueilState extends State<Accueil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
